87fa12e8e6d83cf537f5423a90a375ab43903e50
Luisa Arévalo <larevalo173@gmail.com>
2026-07-07 19:48:09 -0600

    new: Adding indications

---
+++ src/test/java/com/uca/pncparcialfinalhotel/PncParcialFinalHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.pncparcialfinalhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PncParcialFinalHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
